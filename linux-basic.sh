cffc2eb01c6b9fccff9dd5154160b72f533a7cdc
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-08 10:27:50 +1300

    Basic Commands

---
+++ linux-basic.sh
@@ -0,0 +1,23 @@
+# Basic Linux commands (Using Bash)
+
+# Change directory
+cd
+# Move / rename 
+mv
+# Create a directory
+mkdir
+# List directory contents
+ls
+# Remove a file
+rm
+# Remove a directory
+rmdir
+# Copy a file
+cp
+# Copy one or more files
+cat
+# Display x text file 1 screen at a time
+more 
+less
+# Create a file
+touch
