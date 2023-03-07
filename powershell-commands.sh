e600032c1997a9ba995007cc0202c1c5952cd0a8
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-08 10:47:12 +1300

    Added some key powershell commands

---
+++ powershell-commands.sh
@@ -0,0 +1,39 @@
+# Command format:
+# <command> <parameter name> <value>
+# Command is usually a verb-noun pair, e.g. "Get-Process"
+# Example command:
+Get-Process -Name "notepad"
+
+# HANDY COMMANDS
+
+# Get-Command 
+# This command is used to get a list of all available commands.
+# Example:
+Get-Command Get-*
+# This will return a list of all commands that start with "Get-"
+
+# Get-Help
+# This command is used to get help for a command.
+# Example:
+Get-Help Get-Process
+# This will return help for the Get-Process command.
+
+# Clear-Host
+# This command is used to clear the screen.
+# Example:
+Clear-Host
+
+# Set-Location
+# This command is used to change the current directory.
+# Example:
+Set-Location C:\Windows\System32
+# This will change the current directory to C:\Windows\System32
+
+# Get-ChildItem
+# This command is used to list the contents of a directory.
+# Example:
+Get-ChildItem C:\Windows\System32
+# This will list the contents of the C:\Windows\System32 directory.
+
+
+
